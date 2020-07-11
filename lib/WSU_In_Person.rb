cbe3139f59feb665061cb1b6622f966a4677465e
Shin Yamagami <kiboh.usa@gmail.com>
2020-07-10 23:55:32 -0700

    make it read sections

--- lib/WSU_In_Person.rb
+++ lib/WSU_In_Person.rb
@@ -2,7 +2,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
-#require 'pry'
+
 
 module WSUInPerson
   class Error < StandardError; end
@@ -39,35 +39,40 @@ module WSUInPerson
     def scrape_course_pages(subject_urls, prefixes)
 
 
-#        csv << ["row", "of", "CSV", "data"]
- #       csv << ["another", "row"]
 
       csv = CSV.new("output.csv")
       csv = CSV.open("output.csv", "wb")
-      csv << ["Prefix", "Course Title", "Class Number"]
+      csv << ["Prefix", "Course Title", "Section", "Class Number"]
       
       i = 0
   
       subject_urls.each do |subject_url|
         section_urls = []
-  
-        doc = Nokogiri::HTML(open("http://schedules.wsu.edu#{subject_url}"))
-        sections = doc.css('.class_schedule').css('.section').css('a')
-  
+        sections = []
         
-        sections.each do |section|
-          url = section.attribute('href').value
+        doc = Nokogiri::HTML(open("http://schedules.wsu.edu#{subject_url}"))
+        section_links = doc.css('.class_schedule').css('.section').css('a')
+        temp_sections = doc.css('.class_schedule').css('.section').css('a')
+        
+        section_links.each do |section_link|
+          url = section_link.attribute('href').value
           section_urls << url
         end
+
+        temp_sections.each do |temp_section|
+          sections << temp_section.text.strip
+        end
+        
+        
         prefix =  prefixes.at(i)
         i+=1
-        scrape_section_pages(section_urls, prefix, csv)
+        scrape_section_pages(section_urls, prefix, csv, sections)
       end
   
     end
   
-    def scrape_section_pages(section_urls, prefix, csv)
-      in_persons = []
+    def scrape_section_pages(section_urls, prefix, csv, sections)
+      i = 0
   
       section_urls.each do |section_url|
         #puts section_url
@@ -80,14 +85,11 @@ module WSUInPerson
           class_name = doc.at('.sectionInfo').at('b').text.strip
           class_number = doc.at('.sectionInfo')
                            .at("//dt[text()='Class Number']/following-sibling::dd").text
-                           #.text.string
-          puts prefix + "\t" + class_name + "\t" + class_number
-          csv << [prefix, class_name, class_number]
+
+          #puts prefix + "\t" + class_name + "\t" + sections.at(i) + "\t" + class_number
+          csv << [prefix, class_name, sections.at(i), class_number]
         end
-          #sections.each do |secion|
-           # url = section.attribute('href').value
-            #section_urls << url
-      #end
+        i+=1
       end
   
     end
